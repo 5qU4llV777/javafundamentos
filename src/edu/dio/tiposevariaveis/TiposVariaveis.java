4a9478987c6349b63d14d33ef921f0c1e8c84441
5qU4llV777 <claudio.nogueira888@hotmail.com>
2024-10-10 05:53:45 -0400

    update

---
+++ src/edu/dio/tiposevariaveis/TiposVariaveis.java
@@ -0,0 +1,24 @@
+package edu.dio.tiposevariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        String meuNome = "claudio";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        //valor nunca podera mudar , para declarar constante usar final nome da variavel tem que ser todo na caixa alta
+        final double VALOR_DE_PI = 3.14;
+        //não pode ser alterado
+        VALOR_DE_PI = 5.90;
+
+    }
+}
