0296ecde4d7e6339ecb07b45fd3fc03b7d2e2ded
5qU4llV777 <claudio.nogueira888@hotmail.com>
2024-10-12 07:14:34 -0400

    update2

--- src/edu/dio/operadores/Operadores.java
+++ src/edu/dio/operadores/Operadores.java
@@ -1,7 +1,95 @@
 package edu.dio.operadores;
 
+import java.util.function.Function;
+
 public class Operadores {
+
     public static void main(String[] args) {
-        //parei operadores 2
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+            if (condicao1 || condicao2) {
+                System.out.println("uma das condições é verdadeiras");
+
+        }
+        System.out.println("fim");
     }
 }
+/* String nomeUm = "claudio";
+        String nomeDois = new String("claudio");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior a numeroDois? " + simNao);
+
+    }
+}*/
+ /* String nomeCompleto = "linguagem " + "java";
+     System.out.println(nomeCompleto);
+
+     String concatenacao = "?";
+
+     concatenacao = 1+1+1+"1";
+
+     concatenacao = 1+"1"+1+1;
+
+     concatenacao = 1+"1"+1+"1";
+
+     concatenacao = "1"+1+1+1;
+
+     concatenacao = "1"+(1+1+1);
+////////////////////////////////////////////
+       int numero = 5;
+
+    numero = - numero;
+
+    System.out.println(numero);
+    // para converter multiplicar por um
+    numero = numero * -1;
+
+    System.out.println(numero);
+///////////////////////////////////////////////
+    int numero = 5;
+        //x repetição
+        numero =  numero +1;
+        // encrementar como o sinal de ++
+        numero++;
+///////////////////////////////////////////////
+        boolean variavel = true;
+        System.out.println(!variavel);
+
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado =  "falso";
+ ////////////////////////////////////////////
+int a, b;
+
+        a = 6;
+        b = 6;
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+
+*/
