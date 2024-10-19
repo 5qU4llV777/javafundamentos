3cae461b89b2d6987a24390877690e743183c122
5qU4llV777 <claudio.nogueira888@hotmail.com>
2024-10-19 07:18:17 -0400

    update2

--- src/programaçãoOrientadaAObjetos/pilaresPoo/polimorfismo/apps/Telegram.java
+++ src/programaçãoOrientadaAObjetos/pilaresPoo/polimorfismo/apps/Telegram.java
@@ -4,11 +4,13 @@ public class Telegram extends ServicoMensagemInstantanea {
 
     @Override
     public void enviarMensagem() {
+        validarConectadoInternet();
         System.out.println("Enviando mensagem pelo Telegram");
     }
 
     @Override
     public void receberMensagem() {
+
         System.out.println("Recebendo mensagem pelo Telegram");
     }
 }
