50b68c17529769d128df9db6087ad58c9ae4d639
5qU4llV777 <166067521+5qU4llV777@users.noreply.github.com>
2024-11-10 07:45:07 -0300

    Add files via upload

---
+++ estrutura de dados em java/src/test/java/com/cladio/nogueira/NogueiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cladio.nogueira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogueiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
